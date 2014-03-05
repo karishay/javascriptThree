358b7ae0f2a4fd6dc2bb6cf1cd89c62d61bbe9d4
Hackbright Student <user@mini-002.(none)>
2014-03-04 18:00:28 -0800

    Added listener for clicking on list item links which uses AJAX to return the nested things to do list

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var formSubmitButton = $("#form_submit");
-    
+    // button listener that uses ajax to add a new list item 
     formSubmitButton.on("click", function(event){
         event.preventDefault(); // prevent the browser form submission from happening
         $.ajax({
@@ -16,6 +16,7 @@ $(document).ready(function(){
 
     });
 
+    // button listener that uses ajax to add a new to do item to the list item
     var todoSubmitButton = $("#submit-form");
     todoSubmitButton.on("click", function(event){
         event.preventDefault(); // prevent the browser form submission from happening
@@ -30,8 +31,30 @@ $(document).ready(function(){
         }).fail(function(){
             alert('fail!!!');
         });
-
     });
+
+    //button listener that uses ajax to a
+    var seeTodoItems = $("label.todo-list");
+    seeTodoItems.on("click", function(event){
+        event.preventDefault();
+        console.log(event.currentTarget.id);
+
+        // prevent the browser form submission from happening
+        $.ajax({
+            url: '/todo_lists/'+ event.currentTarget.id,
+            method: "GET",
+            //we're gonna need different data
+            data: $("#todo-list").serialize(),
+        }).done(function(data){
+            var listNode = document.getElementsByTagName("body")[0];
+            console.log(listNode);
+            //add a button html in string here
+            listNode.innerHTML = data;
+        }).fail(function(){
+            alert('fail!!!');
+        });
+    });
+
     var getListsFromServer = function() {
         $.ajax({
             url:'/todo_lists/poll',
@@ -43,5 +66,5 @@ $(document).ready(function(){
         });
     };
 
-
 });
+
